a3e832ab57edd93672dd237cfa9f6b518f0e2024
Rinchin <gomboev.rinchin@gmail.com>
2016-06-17 15:01:09 +0300

    Solution

--- Solution.java
+++ Solution.java
@@ -2,6 +2,7 @@ package com.javarush.test.level09.lesson11.home04;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -16,5 +17,18 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        SimpleDateFormat inputFormat = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat outputFormat = new SimpleDateFormat("MMM dd, yyyy",Locale.ENGLISH);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try
+        {
+            Date date = inputFormat.parse(reader.readLine());
+            System.out.println(outputFormat.format(date).toUpperCase());
+        }
+        catch (ParseException e)
+        {
+            System.out.println(e);
+        }
     }
 }
